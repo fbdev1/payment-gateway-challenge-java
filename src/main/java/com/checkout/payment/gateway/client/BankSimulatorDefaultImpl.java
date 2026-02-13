e234366539650ae23fb4df4b86ee77968b15cf0d
vsyrkin <viktor.syrkin@telekom.com>
2026-02-13 16:39:42 +0200

    minor fix of simulator logic

--- src/main/java/com/checkout/payment/gateway/client/BankSimulatorDefaultImpl.java
+++ src/main/java/com/checkout/payment/gateway/client/BankSimulatorDefaultImpl.java
@@ -48,12 +48,11 @@ public class BankSimulatorDefaultImpl implements BankSimulatorInterface {
       BankPaymentResponse body = response.getBody();
       return handleSuccessBankResponse(body, paymentId);
 
-    }
-    catch(AcquiringProcessException ex){
+    } catch (AcquiringProcessException ex) {
       throw ex;
-    }
-    catch (Exception e) {
-      LOG.error("Bank simulator respond with error while making payment request for payment ID: {}, lastFour: {}",
+    } catch (Exception e) {
+      LOG.error(
+          "Bank simulator respond with error while making payment request for payment ID: {}, lastFour: {}",
           paymentId, request.getCardNumberLastFour(), e);
       throw new AcquiringProcessException("Bank simulator respond with error while making payment "
           + "request for payment ID: " + paymentId);
@@ -65,20 +64,17 @@ public class BankSimulatorDefaultImpl implements BankSimulatorInterface {
       LOG.warn("Bank returned null body for payment ID: {}", paymentId);
       throw new AcquiringProcessException("Bank returned null body for payment ID: " + paymentId);
     }
-
-    if (response.authorizationCode() == null) {
-      LOG.warn("Bank returned empty authorization code for payment ID: {}", paymentId);
-      throw new AcquiringProcessException(
-          "Bank returned empty authorization code for payment ID: " + paymentId);
+    if (response.authorized() && response.authorizationCode() != null && !response.authorizationCode().isEmpty()) {
+      LOG.info("Bank authorized current operation with payment ID: {} with authorization code: {}",
+          paymentId, response.authorizationCode());
+      return PaymentStatus.AUTHORIZED;
     }
-
     if (!response.authorized()) {
       LOG.info("Bank declined current operation with payment ID: {}", paymentId);
       return PaymentStatus.DECLINED;
     }
-
-    LOG.info("Bank authorized current operation with payment ID: {} with authorization code: {}",
-        paymentId, response.authorizationCode());
-    return PaymentStatus.AUTHORIZED;
+    LOG.warn("Bank returned empty authorization code for payment ID: {}", paymentId);
+    throw new AcquiringProcessException(
+        "Bank returned empty authorization code for payment ID: " + paymentId);
   }
 }
